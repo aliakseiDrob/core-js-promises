24f023200d1cbc64358b647fa8936a5c79ab247a
aliakseidrob <aliakseidrob@gmail.com>
2026-01-28 22:01:41 +0300

    feat: solution getAllResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -110,8 +110,11 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+async function getAllResult(promises) {
+  const resultArray = await Promise.allSettled(promises);
+  return [...resultArray].map((result) => {
+    return result.value || null;
+  });
 }
 
 /**
